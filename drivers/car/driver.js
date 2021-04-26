b0d48be5a1e2f7dbea84a60307af4b3fdf29e383
AdyRock <38184396+AdyRock@users.noreply.github.com>
2021-04-26 16:35:36 +0100

    Added any window open condition card

--- drivers/car/driver.js
+++ drivers/car/driver.js
@@ -13,13 +13,29 @@ module.exports = class MercedesMeDriver extends OAuth2Driver
         this.vehicleIsUnlocked = this.homey.flow.getDeviceTriggerCard('vehicle_unlocked');
 
         // Register Condition Flow Cards.
-        let wateringCondition = this.homey.flow.getConditionCard('is_locked');
-        wateringCondition.registerRunListener(async (args, state) =>
+        let isLockedCondition = this.homey.flow.getConditionCard('is_locked');
+        isLockedCondition.registerRunListener(async (args, state) =>
         {
             const lock_state = args.device.getCapabilityValue('doorlockstatusvehicle');
             return ((lock_state === this.homey.__('doorlockstatusvehicle.1')) || (lock_state === this.homey.__('doorlockstatusvehicle.2'))); // true or false
         });
 
+        // Register Condition Flow Cards.
+        let isOpenCondition = this.homey.flow.getConditionCard('is_open');
+        isOpenCondition.registerRunListener(async (args, state) =>
+        {
+            const sunroof_state = args.device.getCapabilityValue('sunroofstatus');
+            const window1_state = args.device.getCapabilityValue('windowstatusfrontleft');
+            const window2_state = args.device.getCapabilityValue('windowstatusfrontright');
+            const window3_state = args.device.getCapabilityValue('windowstatusrearleft');
+            const window4_state = args.device.getCapabilityValue('windowstatusrearright');
+
+            return ((sunroof_state !== this.homey.__('sunroofstatus.0')) || // Check for not closed
+                    (window1_state !== this.homey.__('windowstatus.2')) ||
+                    (window2_state !== this.homey.__('windowstatus.2')) ||
+                    (window3_state !== this.homey.__('windowstatus.2')) ||
+                    (window4_state !== this.homey.__('windowstatus.2'))); // Return true if anything is not closed
+        });
     }
 
     checkTrigger(device, key, oldValue, newValue)
